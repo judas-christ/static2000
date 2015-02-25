dbbff759e66e1e8c765035627f46f44f8e590ae0
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2015-02-25 13:19:45 +0100

    added travis file and editorconfig

--- gulpfile.js
+++ gulpfile.js
@@ -9,22 +9,22 @@ var del = require('del');
 var src = ['index.js','lib/*.js'];
 
 gulp.task('jshint', function() {
-    return gulp.src(src)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-        .on('error', gutil.log);
+  return gulp.src(src)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .on('error', gutil.log);
 });
 
 gulp.task('mocha', function() {
-    return gulp.src('test/**/*.spec.js', {read: false})
-        .pipe(mocha())
-        .on('error', gutil.log);
+  return gulp.src('test/**/*.spec.js', {read: false})
+    .pipe(mocha())
+    .on('error', gutil.log);
 });
 
 gulp.task('clean', function(cb) {
-    del('www', cb);
+  del('www', cb);
 });
 
 gulp.task('default', ['jshint', 'mocha'], function() {
-    gulp.watch(['index.js','lib/*.js','test/*.js'], ['jshint', 'mocha']);
+  gulp.watch(['index.js','lib/*.js','test/*.js'], ['jshint', 'mocha']);
 });
