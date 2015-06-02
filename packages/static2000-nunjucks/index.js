c3302f70b814eb2891f15536fc83ffc606d22097
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2015-06-02 21:33:19 +0200

    and the actual fix...

--- packages/static2000-nunjucks/index.js
+++ packages/static2000-nunjucks/index.js
@@ -4,19 +4,25 @@
 var nunjucks = require('nunjucks');
 var path = require('path');
 
+function createEnvironment(templatesPath) {
+  return nunjucks.configure(templatesPath, {
+    watch: false
+  });
+}
+
 module.exports = {
-    templateExtension: '.html',
-    templateGlob: '*.html',
-    globalsInclude: '{% import "./includes/globals.html" as globals %}\n',
-    compile: function(source, options) {
-        var templatesPath = path.dirname(options.filename);
-        var compiled = nunjucks.compile(source, new nunjucks.Environment([new nunjucks.FileSystemLoader(templatesPath)]), options.filename);
-        return function(options) {
-            return compiled.render(options);
-        };
-    },
-    render: function(source, options) {
-        var templatesPath = path.dirname(options.filename);
-        return nunjucks.compile(source, new nunjucks.Environment([new nunjucks.FileSystemLoader(templatesPath)]), options.filename).render(options);
-    }
+  templateExtension: '.html',
+  templateGlob: '*.html',
+  globalsInclude: '{% import "./includes/globals.html" as globals %}\n',
+  compile: function(source, options) {
+    var templatesPath = path.dirname(options.filename);
+    var compiled = nunjucks.compile(source, createEnvironment(templatesPath), options.filename);
+    return function(options) {
+      return compiled.render(options);
+    };
+  },
+  render: function(source, options) {
+    var templatesPath = path.dirname(options.filename);
+    return nunjucks.compile(source, createEnvironment(templatesPath), options.filename).render(options);
+  }
 };
