185354286d69391bfc4a2a9dbd98d0efc5c9317b
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-27 22:06:39 +0200

    Added some tests

---
+++ packages/static2000-pug/test/index.spec.js
@@ -0,0 +1,59 @@
+const pugAdapter = require('../index');
+var should = require('should');
+
+describe('pug adapter', function() {
+
+  it('should render simple template', function() {
+    var rendered = pugAdapter.render('p Test string', {});
+    rendered.should.equal('<p>Test string</p>');
+  });
+
+  it('should compile simple template', function() {
+    var compiled = pugAdapter.compile('p Test string', {
+      filename: 'test/templates/simple-template.pug'
+    });
+    var rendered = compiled({});
+    rendered.trim().should.equal('<p>Test string</p>'); //have to trim because prepended newline
+  });
+
+  it('should use locals', function() {
+    var compiled = pugAdapter.compile('p= variable', {
+      filename: 'test/templates/simple-template.pug'
+    });
+    var rendered = compiled({
+      variable: 'Test string'
+    });
+    rendered.trim().should.equal('<p>Test string</p>');
+  });
+
+  it('should extend a layout', function() {
+    var compiled = pugAdapter.compile(`extends ./layouts/default.pug
+block title
+  | Test title
+
+block body
+  | Test body`, {
+      filename: 'test/templates/use-layout.pug'
+    });
+    var rendered = compiled({});
+    rendered.trim().should.equal(`<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test title
+    </title>
+  </head>
+  <body>Test body
+  </body>
+</html>`);
+  });
+
+  it('should use mixins', function() {
+    var template = pugAdapter.globalsInclude + '+test-mixin';
+    var compiled = pugAdapter.compile(template, {
+      filename: 'test/templates/mixin-template.pug'
+    });
+    var rendered = compiled({});
+    rendered.trim().should.equal('<p>Test mixin</p>');
+  });
+});
