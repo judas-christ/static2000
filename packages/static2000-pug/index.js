0cd18d2458b6ba07885a5b7d3ef904e9e5bb3318
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2018-02-03 10:52:28 +0100

    Fixed static2000 plugins interfering with pug plugins

--- packages/static2000-pug/index.js
+++ packages/static2000-pug/index.js
@@ -3,6 +3,12 @@
 
 var pug = require('pug');
 
+function renamePlugins(source, options) {
+    options.plugins2000 = options.plugins
+    delete options.plugins
+    return source.replace(/plugins\./g, 'plugins2000');
+}
+
 module.exports = {
     templateExtension: '.pug',
     templateGlob: '*.pug',
@@ -10,6 +16,7 @@ module.exports = {
     compile: function(source, options) {
         // pug requires extends to come first
         source = source.replace(new RegExp(this.globalsInclude + '(extends [^\\n]+\n)'), `$1${this.globalsInclude}`);
+        source = renamePlugins(source, options)
         var pugOptions = {
             filename: options.filename,
             pretty: true
@@ -17,6 +24,7 @@ module.exports = {
         return pug.compile(source, pugOptions);
     },
     render: function(source, options) {
+        source = renamePlugins(source, options)
         return pug.render(source, options);
     }
 };
